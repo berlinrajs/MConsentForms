3cddb0950c2fbaa047dd94c2481abea369526016
Berlin Raj <srswebsolutions@berlins-mac-mini.local>
2016-07-14 11:45:21 +0530

    Initial Commit

---
+++ MConsentForms/MConsentForms.h
@@ -0,0 +1,19 @@
+//
+//  MConsentForms.h
+//  MConsentForms
+//
+//  Created by SRS Web Solutions on 14/07/16.
+//  Copyright © 2016 SRS Web Solutions. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MConsentForms.
+FOUNDATION_EXPORT double MConsentFormsVersionNumber;
+
+//! Project version string for MConsentForms.
+FOUNDATION_EXPORT const unsigned char MConsentFormsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MConsentForms/PublicHeader.h>
+
+
